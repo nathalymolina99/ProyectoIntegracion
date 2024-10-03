4a3f2998332c63ef1c8f88ad23b5762cb2cae56b
nathalymolina99 <nathaly.molina02@inacapmail.cl>
2024-10-03 04:30:02 -0300

    comentario temporal
    Comente eso en privateRoute para que se puedan ingresar a las urls directo

--- src/components/privateRoute.js
+++ src/components/privateRoute.js
@@ -5,15 +5,15 @@ import { AuthContext } from '../context/authContext';
 const PrivateRoute = ({ children, role }) => {
   const { auth } = useContext(AuthContext);
   
-  if (!auth) {
-    // Si no está autenticado, redirigir al login
-    return <Navigate to="/login" />;
-  }
+  // if (!auth) {
+  //   // Si no está autenticado, redirigir al login
+  //   return <Navigate to="/login" />;
+  // }
   
-  if (role && auth.role !== role) {
-    // Si el rol no coincide, redirigir a la página de bienvenida o a otra página de acceso denegado
-    return <Navigate to="/" />;
-  }
+  // if (role && auth.role !== role) {
+  //   // Si el rol no coincide, redirigir a la página de bienvenida o a otra página de acceso denegado
+  //   return <Navigate to="/" />;
+  // }
   
   return children;
 };
